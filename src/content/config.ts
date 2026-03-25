2bc50b3c1f3df1337e93ea81847dffa593518133
okmt <kohei020213@gmail.com>
2026-03-25 14:48:02 +0900

    Add gallery page

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from 'astro:content';
+const work3d = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    video: z.string(),
+    images: z.array(z.string()).optional(),
+  }),
+});
+const work2d = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+  }),
+});
+export const collections = {
+  '3d': work3d,
+  '2d': work2d,
+};
